5ecc2d7335dc73e0ec3c338c31a3773cc57e81d2
Benjamin Blankenmeister <bblanken@broadinstitute.org>
2023-11-14 12:44:25 -0500

    Add some bare bones unittests

---
+++ unit-test/test_seqr_chart.py
@@ -0,0 +1,35 @@
+import os
+import subprocess
+import unittest
+
+WORK_DIR = os.path.dirname(__file__)
+DEFAULT_ARGS = ['helm', 'template', 'test-seqr', 'charts/seqr',  '-f', os.path.join(WORK_DIR, 'values.yaml')]
+
+class TestSeqrChart(unittest.TestCase):
+
+    def test_values(self):
+        p = subprocess.run(DEFAULT_ARGS, capture_output=True, text=True) # NB: text=True here to avoid opening the output in binary mode
+        p.check_returncode()
+        self.assertIn('kind: Deployment\nmetadata:\n  name: seqr', p.stdout)
+        self.assertIn('echo starting CronJob test-cron-1;', p.stdout)
+        self.assertIn('iam.gke.io/gcp-service-account: test-service-account@developer.gserviceaccount.com', p.stdout)
+        self.assertIn('cloud-sql-proxy', p.stdout)
+        self.assertIn('serviceAccountName: test-seqr', p.stdout)
+
+    def test_no_deployment_sidecars(self):
+        p = subprocess.run([*DEFAULT_ARGS, '-f', os.path.join(WORK_DIR, 'no-deployment-sidecars.yaml')], capture_output=True, text=True)
+        p.check_returncode()
+        self.assertNotIn('cloud-sql-proxy', p.stdout)
+
+    def test_no_service_account(self):
+        p = subprocess.run([*DEFAULT_ARGS, '-f', os.path.join(WORK_DIR, 'no-service-account.yaml')], capture_output=True, text=True)
+        p.check_returncode()
+        self.assertNotIn('serviceAccountName: test-seqr\n', p.stdout)
+
+    def test_incorrectly_formatted_cronjob(self):
+        p = subprocess.run([*DEFAULT_ARGS, '-f', os.path.join(WORK_DIR, 'incorrectly-formatted-cronjob.yaml')], capture_output=True, text=True)
+        self.assertRaises(subprocess.CalledProcessError, p.check_returncode)
+        self.assertIn("can't evaluate field name in type interface", p.stderr)
+
+if __name__ == '__main__':
+    unittest.main()

871589b63001b5ff38cc2bf2be89cafe9803e0ec
Benjamin Blankenmeister <bblanken@broadinstitute.org>
2024-01-09 16:37:00 -0500

    Fix tests

--- unit_test/hail_search/test_hail_search_chart.py
+++ unit_test/hail_search/test_hail_search_chart.py
@@ -20,8 +20,8 @@ class TestHailSearchChart(unittest.TestCase):
         p = subprocess.run(DEFAULT_ARGS, capture_output=True, text=True) # NB: text=True here to avoid opening the output in binary mode
         p.check_returncode()
         self.assertIn('serviceAccountName: test-hail-search', p.stdout)
-        self.assertIn('name: sync-projects-grch38-snv-indel', p.stdout)
-        self.assertIn('name: sync-families-grch38-snv-indel', p.stdout)
+        self.assertIn('name: sync-grch38-snv-indel', p.stdout)
+        self.assertIn('name: sync-ssd-grch38-snv-indel', p.stdout)
         self.assertIn('checksum/datasetVersions', p.stdout)
         self.assertIn('checksum/config', p.stdout)
 

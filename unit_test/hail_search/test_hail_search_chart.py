48158bd24f7fda885205126cb72773ff5552b7b1
Benjamin Blankenmeister <bblanken@broadinstitute.org>
2024-03-27 15:42:00 -0400

    Make sure it's optional

--- unit_test/hail_search/test_hail_search_chart.py
+++ unit_test/hail_search/test_hail_search_chart.py
@@ -21,11 +21,16 @@ class TestHailSearchChart(unittest.TestCase):
         p.check_returncode()
         self.assertIn('serviceAccountName: test-hail-search', p.stdout)
         self.assertIn('name: sync-annotations-grch38-snv-indel', p.stdout)
-        self.assertIn("command: ['mkdir', '-p', '/datasets/GRCh38/SV_WES', '&&', 'rsync', '-r', '/ssd-datasets/GRCh38/SV_WES/annotations.ht', '/datasets/GRCh38/SV_WES/annotations.ht']", p.stdout)
-        self.assertIn('volumeHandle: projects/test-project/zones/us-central3-a/disks/test-disk', p.stdout)
         self.assertIn('checksum/datasetVersions', p.stdout)
         self.assertIn('checksum/config', p.stdout)
+        self.assertNotIn('claimName: test-hail-search-pvc', p.stdout)
 
+    def test_persistent_volume(self):
+        p = subprocess.run([*DEFAULT_ARGS, '-f', os.path.join(WORK_DIR, 'persistentvolume.yaml')], capture_output=True, text=True)
+        p.check_returncode()
+        self.assertIn('claimName: test-hail-search-pvc', p.stdout)
+        self.assertIn("command: ['mkdir', '-p', '/datasets/GRCh38/SV_WES', '&&', 'rsync', '-r', '/ssd-datasets/GRCh38/SV_WES/annotations.ht', '/datasets/GRCh38/SV_WES/annotations.ht']", p.stdout)
+        self.assertIn('volumeHandle: projects/test-project/zones/us-central3-a/disks/test-disk', p.stdout)
 
 if __name__ == '__main__':
     unittest.main()

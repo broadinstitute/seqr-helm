09a4d6fd4bf553f86e33fc9b30449b70fed48d61
Benjamin Blankenmeister <bblanken@broadinstitute.org>
2024-08-27 17:39:40 -0400

    whack on tests

--- unit_test/hail_search/test_hail_search_chart.py
+++ unit_test/hail_search/test_hail_search_chart.py
@@ -6,7 +6,7 @@ WORK_DIR = os.path.dirname(__file__)
 DEFAULT_ARGS = [
     'helm',
     'install',
-    'test-hail-search', 
+    'test', 
     'charts/hail-search', 
     '--dry-run',
     '--debug', 
@@ -19,17 +19,17 @@ class TestHailSearchChart(unittest.TestCase):
     def test_values(self):
         p = subprocess.run(DEFAULT_ARGS, capture_output=True, text=True) # NB: text=True here to avoid opening the output in binary mode
         p.check_returncode()
-        self.assertIn('serviceAccountName: test-hail-search', p.stdout)
+        self.assertIn('serviceAccountName: hail-search', p.stdout)
         self.assertIn('name: sync-annotations-grch38-snv-indel', p.stdout)
         self.assertIn('checksum/datasetVersions', p.stdout)
         self.assertIn('checksum/config', p.stdout)
         self.assertIn('a/deployment', p.stdout)
-        self.assertNotIn('claimName: test-hail-search-pvc-9ff0a', p.stdout)
+        self.assertNotIn('claimName: hail-search-pvc-9ff0a', p.stdout)
 
     def test_persistent_volume(self):
         p = subprocess.run([*DEFAULT_ARGS, '-f', os.path.join(WORK_DIR, 'persistentvolume.yaml')], capture_output=True, text=True)
         p.check_returncode()
-        self.assertIn('claimName: test-hail-search-pvc-9ff0a', p.stdout)
+        self.assertIn('claimName: hail-search-pvc-9ff0a', p.stdout)
         self.assertIn("command: ['/bin/sh', '-c', 'mkdir -p /datasets/GRCh38/SV_WES && cp -r /ssd-datasets/GRCh38/SV_WES/annotations.ht /datasets/GRCh38/SV_WES/annotations.ht']", p.stdout)
         self.assertIn('volumeHandle: projects/test-project/zones/us-central3-a/disks/test-disk', p.stdout)
 
--- unit_test/seqr/test_seqr_chart.py
+++ unit_test/seqr/test_seqr_chart.py
@@ -24,11 +24,11 @@ class TestSeqrChart(unittest.TestCase):
     def test_values(self):
         p = subprocess.run(DEFAULT_ARGS, capture_output=True, text=True)
         p.check_returncode()
-        self.assertIn('kind: Deployment\nmetadata:\n  name: test-seqr', p.stdout)
+        self.assertIn('kind: Deployment\nmetadata:\n  name: seqr', p.stdout)
         self.assertIn('echo starting CronJob test-cron-1;', p.stdout)
         self.assertIn('iam.gke.io/gcp-service-account: test-service-account@developer.gserviceaccount.com', p.stdout)
         self.assertIn('cloud-sql-proxy', p.stdout)
-        self.assertIn('serviceAccountName: test-seqr', p.stdout)
+        self.assertIn('serviceAccountName: seqr', p.stdout)
         self.assertIn('SEQR_ES_PASSWORD', p.stdout)
         self.assertIn('a/deployment', p.stdout)
         self.assertEqual(p.stdout.count('kind: CronJob'), 2)
@@ -41,12 +41,12 @@ class TestSeqrChart(unittest.TestCase):
     def test_no_service_account(self):
         p = subprocess.run([*DEFAULT_ARGS, '-f', os.path.join(WORK_DIR, 'no-service-account.yaml')], capture_output=True, text=True)
         p.check_returncode()
-        self.assertNotIn('serviceAccountName: test-seqr\n', p.stdout)
+        self.assertNotIn('serviceAccountName: seqr\n', p.stdout)
 
     def test_incorrectly_formatted_cronjob(self):
         p = subprocess.run([*DEFAULT_ARGS, '-f', os.path.join(WORK_DIR, 'incorrectly-formatted-cronjob.yaml')], capture_output=True, text=True)
         self.assertRaises(subprocess.CalledProcessError, p.check_returncode)
-        self.assertIn('invalid resource name "test-seqr-a/bad/cron/1": [may not contain \'/\']\nhelm.go', p.stderr)
+        self.assertIn('invalid resource name "seqr-a/bad/cron/1": [may not contain \'/\']\nhelm.go', p.stderr)
 
     def test_check_new_samples_job(self):
         p = subprocess.run([*DEFAULT_ARGS, '-f', os.path.join(WORK_DIR, 'check-new-samples-job.yaml')], capture_output=True, text=True)

966d05ef913bfbb09ad58268a65c65caa3b79ccb
Benjamin Blankenmeister <bblanken@broadinstitute.org>
2024-09-04 14:34:03 -0400

    Remove datasetVersions

--- unit_test/hail_search/test_hail_search_chart.py
+++ unit_test/hail_search/test_hail_search_chart.py
@@ -21,12 +21,12 @@ class TestHailSearchChart(unittest.TestCase):
         p.check_returncode()
         self.assertIn('serviceAccountName: hail-search', p.stdout)
         self.assertIn('name: sync-annotations-grch38-snv-indel', p.stdout)
-        self.assertIn('checksum/datasetVersions: a3455105c5ed96d20f9422cbcf71ab28e9057ced7e048c8892ffaf721e5b7946', p.stdout)
         self.assertIn('checksum/config', p.stdout)
         self.assertIn('a/deployment', p.stdout)
         self.assertIn('claimName: hail-search-pvc', p.stdout)
         self.assertIn('local:', p.stdout)
         self.assertNotIn('9ff0a', p.stdout)
+        self.assertNotIn('checksum/datasetVersions', p.stdout)
 
     def test_persistent_volume(self):
         p = subprocess.run([*DEFAULT_ARGS, '-f', os.path.join(WORK_DIR, 'persistentvolume.yaml')], capture_output=True, text=True)
@@ -34,6 +34,7 @@ class TestHailSearchChart(unittest.TestCase):
         self.assertIn('claimName: hail-search-pvc-9ff0a', p.stdout)
         self.assertIn("command: ['/bin/sh', '-c', 'mkdir -p /datasets/GRCh38/SV_WES && cp -r /ssd-datasets/GRCh38/SV_WES/annotations.ht /datasets/GRCh38/SV_WES/annotations.ht']", p.stdout)
         self.assertIn('volumeHandle: projects/test-project/zones/us-central3-a/disks/test-disk', p.stdout)
+        self.assertIn('checksum/datasetVersions', p.stdout)
 
 if __name__ == '__main__':
     unittest.main()

7107e0aaf06ae4d068a21a03549ef22ba25d3722
Benjamin Blankenmeister <bblanken@broadinstitute.org>
2024-09-23 18:39:01 -0400

    fix test

--- unit_test/hail_search/test_hail_search_chart.py
+++ unit_test/hail_search/test_hail_search_chart.py
@@ -24,12 +24,12 @@ class TestHailSearchChart(unittest.TestCase):
         self.assertIn('checksum/config', p.stdout)
         self.assertIn('a/deployment', p.stdout)
         self.assertNotIn('name: sync-annotations-grch38-snv-indel', p.stdout)
-        self.assertNotIn('claimName: hail-search-pvc-9ff0a', p.stdout)
+        self.assertNotIn('claimName: seqr-platform-pvc-9ff0a', p.stdout)
 
     def test_persistent_volume(self):
         p = subprocess.run([*DEFAULT_ARGS, '-f', os.path.join(WORK_DIR, 'persistentvolume.yaml')], capture_output=True, text=True)
         p.check_returncode()
-        self.assertIn('claimName: hail-search-pvc-9ff0a', p.stdout)
+        self.assertIn('claimName: seqr-platform-pvc-9ff0a', p.stdout)
         self.assertIn("command: ['/bin/sh', '-c', 'mkdir -p /in-memory-datasets/GRCh38/SV_WES && cp -r /seqr/seqr-hail-search-data/GRCh38/SV_WES/annotations.ht /in-memory-datasets/GRCh38/SV_WES/annotations.ht']", p.stdout)
         self.assertIn('volumeHandle: projects/test-project/zones/us-central3-a/disks/test-disk', p.stdout)
 

482da2cc0024383a2cec47e6b92780c0e8ce3f1b
Benjamin Blankenmeister <bblanken@broadinstitute.org>
2024-09-25 09:26:36 -0400

    A few more tweaks

--- unit_test/pipeline-runner/test_pipeline_runner_chart.py
+++ unit_test/pipeline-runner/test_pipeline_runner_chart.py
@@ -14,17 +14,15 @@ DEFAULT_ARGS = [
     os.path.join(WORK_DIR, 'values.yaml')
 ]
 
-class TestHailSearchChart(unittest.TestCase):
+class TestPipelineRunnerChart(unittest.TestCase):
 
     def test_values(self):
         p = subprocess.run(DEFAULT_ARGS, capture_output=True, text=True) # NB: text=True here to avoid opening the output in binary mode
         p.check_returncode()
-        self.assertIn('serviceAccountName: hail-search', p.stdout)
-        self.assertIn('checksum/datasetVersions', p.stdout)
+        self.assertIn('serviceAccountName: pipeline-runner', p.stdout)
         self.assertIn('checksum/config', p.stdout)
-        self.assertIn('a/deployment', p.stdout)
-        self.assertNotIn('name: sync-annotations-grch38-snv-indel', p.stdout)
-        self.assertNotIn('claimName: seqr-platform-pvc-9ff0a', p.stdout)
+        self.assertIn('pipeline-runner-ui', p.stdout)
+        self.assertIn('pipeline-runner-api-sidecar', p.stdout)
 
 if __name__ == '__main__':
     unittest.main()
